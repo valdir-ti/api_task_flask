b7653860b33e9e9b722b1287a619c46162056764
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-05 15:51:33 -0300

    feat: add json response and a swagger file

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World"
+    return jsonify({
+        'message': 'API is running!'
+    })
 
 @app.route('/about')
 def about():
-    return "About page"
+    return jsonify({
+        'message': 'About Page'
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
