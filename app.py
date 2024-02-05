efa16660b4eff7db94710a86d623a683d104aad2
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-05 10:36:04 -0300

    feat: create first routes

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("API flask")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+@app.route('/about')
+def about():
+    return "About page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
