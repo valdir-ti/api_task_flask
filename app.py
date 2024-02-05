8339f3721e931cdab23ac507b3c2458559330066
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-05 19:48:39 -0300

    feat: delete task

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def update_task(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
     
     if task == None:
         return jsonify({'message': 'task not found'}, 404)
@@ -67,6 +68,19 @@ def update_task(id):
     return jsonify({'message': 'Tarefa atualizada com sucesso'})
 
 # DELETE
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    
+    if not task:
+        return jsonify({'message': 'task not found'}, 404)
+    
+    tasks.remove(task)    
+    return jsonify({'message': 'Tarefa deletada com sucesso'})
 
 if __name__ == '__main__':
     app.run(debug=True)
